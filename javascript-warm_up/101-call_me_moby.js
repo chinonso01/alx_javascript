82fa724ce0b6a148d809e66efa5fac848e097f3c
chinonso01 <irobindachinonso>
2024-01-30 12:37:59 +0100

    commit

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,7 @@
-function executeXTimes(x, theFunction) {
+function callMeMoby(x, theFunction) {
     for (let i = 0; i < x; i++) {
       theFunction();
     }
-}
+  }
+  
+  module.exports.callMeMoby = callMeMoby;
