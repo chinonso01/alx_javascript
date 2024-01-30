1cabac564a0ad17351ef0ee88e844f7dd0a764b6
chinonso01 <irobindachinonso>
2024-01-30 14:05:22 +0100

    commit

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,13 +2,12 @@
 const myObject = {
     type: 'object',
     value: 12,
-    incr: function() {
-      this.value += 1;
-    }
 };
   
 console.log(myObject);
-
+myObject.incr = function() {
+    this.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
