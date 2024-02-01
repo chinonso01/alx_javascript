9ff4c8e994da25d83eff817223c513a577a3aae2
chinonso01 <irobindachinonso>
2024-02-01 13:02:03 +0100

    commit

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// 3-rectangle.js
+class Rectangle {
+    constructor(w, h){
+        if (w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+        else {
+            Object.create(null);
+        }
+    }
+
+    print() {
+        if (this.width && this.height) {
+          for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+          }
+        }
+    }
+}
+
+module.exports = Rectangle;
