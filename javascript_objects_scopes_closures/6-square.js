715a03715b01774006ef6f939c4c3177fd39ca3e
chinonso01 <irobindachinonso>
2024-02-01 13:20:29 +0100

    commit

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -35,11 +35,6 @@ class Rectangle {
     }
 }
 
-class Square extends Rectangle {
-    constructor(size){
-        super(size, size);
-    }
-}
 
 class Square extends Square {
     constructor(size){
