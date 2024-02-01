fa4b31148a920adfee88f56d34a0f95c38512e93
chinonso01 <irobindachinonso>
2024-02-01 13:19:17 +0100

    commit

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,60 @@
+#!/usr/bin/node
+// 6-rectangle.js
+class Rectangle {
+    constructor(w, h){
+        if (w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+        else {
+            Object.create(null);
+        }
+    }
+
+    print() {
+        if (this.width && this.height) {
+          for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+          }
+        }
+    }
+
+    rotate() {
+        if (this.width && this.height) {
+          const temp = this.width;
+          this.width = this.height;
+          this.height = temp;
+        }
+    }
+
+    double() {
+        if (this.width && this.height) {
+          this.width *= 2;
+          this.height *= 2;
+        }
+    }
+}
+
+class Square extends Rectangle {
+    constructor(size){
+        super(size, size);
+    }
+}
+
+class Square extends Square {
+    constructor(size){
+        super(size, size);
+    }
+
+    charPrint(c) {
+        if (c === undefined) {
+          c = 'X';
+        }
+    
+        for (let i = 0; i < this.height; i++) {
+          console.log(c.repeat(this.width));
+        }
+    }
+}
+
+module.exports = Square;
