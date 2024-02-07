60c33eed8eb47f994c600c7c8387e2c87e826e0e
chinonso01 <irobindachinonso>
2024-02-07 16:42:24 +0100

    commit

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -11,19 +11,15 @@ request.get(url, (error, response, body) => {
         // If there's an error, print it
         console.error(error);
     } else {
-        if (response.statusCode === 200) {
-            // Write the body response to the file
-            fs.writeFile(filePath, body, 'utf-8', (err) => {
-                if (err) {
-                    // If there's an error writing to the file, print it
-                    console.error(err);
-                } else {
-                    // Print a success message
-                    console.log(`Content successfully saved to ${filePath}`);
-                }
-            });
-        } else {
-            console.error(`Failed to fetch content from ${url}. Status code: ${response.statusCode}`);
-        }
+        // Write the body response to the file
+        fs.writeFile(filePath, body, 'utf-8', (err) => {
+            if (err) {
+                // If there's an error writing to the file, print it
+                console.error(err);
+            } else {
+                // Print a success message
+                console.log(`Content successfully saved to ${filePath}`);
+            }
+        });
     }
 });
