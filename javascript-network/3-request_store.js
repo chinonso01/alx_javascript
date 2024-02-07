361bacf240a1cb13296fe7cdd9254e4b63b7cd99
chinonso01 <irobindachinonso>
2024-02-07 16:32:12 +0100

    commit

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const fs = require('fs');
+
+// Get the URL and file path from the command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a GET request to the URL
+request.get(url, (error, response, body) => {
+    if (error) {
+        // If there's an error, print it
+        console.error(error);
+    } else {
+        // Write the body response to the file
+        fs.writeFile(filePath, body, 'utf-8', (err) => {
+            if (err) {
+                // If there's an error writing to the file, print it
+                console.error(err);
+            } else {
+                // Print a success message
+                console.log(`Content successfully saved to ${filePath}`);
+            }
+        });
+    }
+});
